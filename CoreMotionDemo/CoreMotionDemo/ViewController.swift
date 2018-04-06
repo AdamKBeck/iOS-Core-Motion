72ee71939b976767c4c761bf230c0342095b3fdd
Adam Beck <akb93@case.edu>
2018-04-06 11:01:57 -0400

    Create gitignore and blank project

---
+++ CoreMotionDemo/CoreMotionDemo/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  CoreMotionDemo
+//
+//  Created by Adam Beck on 4/6/18.
+//  Copyright © 2018 Adam Beck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
