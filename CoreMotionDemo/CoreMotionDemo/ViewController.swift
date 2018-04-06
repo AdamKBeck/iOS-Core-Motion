6d75b49442580d3c793fe17a21e28af83452a034
Adam Beck <akb93@case.edu>
2018-04-06 11:23:24 -0400

    Add storyboard labels

--- CoreMotionDemo/CoreMotionDemo/ViewController.swift
+++ CoreMotionDemo/CoreMotionDemo/ViewController.swift
@@ -1,15 +1,25 @@
-//
-//  ViewController.swift
-//  CoreMotionDemo
-//
-//  Created by Adam Beck on 4/6/18.
-//  Copyright © 2018 Adam Beck. All rights reserved.
-//
-
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
-
+    // Motion manager
+    var motionManager = CMMotionManager()
+    
+    // Variables to hold data while the app is running
+    var maxAccX = 0.0
+    var maxAccY = 0.0
+    var maxAccZ = 0.0
+    var maxRotX = 0.0
+    var maxRotY = 0.0
+    var maxRotZ = 0.0
+    
+    // Outlets
+    
+    
+    
+    
+    
+    // Built-in functions
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -17,6 +27,9 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    
+    // Functions
 
 
 }
